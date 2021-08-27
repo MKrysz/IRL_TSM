b8c698455d54da367d79a20e1dbbaa07c917f371
MKrysz <mkrysz@student.agh.edu.pl>
2021-08-27 20:30:04 +0200

    Added function declarations, added global variable containing names of all the tables

--- PYTHON/IRL_TSM_database.py
+++ PYTHON/IRL_TSM_database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+tables = ("products", "receipts", "purchases")
 databasePath = r'..\Databases\IRL_TSM.db'
 
 def customCommand(command):
@@ -12,7 +13,7 @@ def customCommand(command):
 def createDatabase():
     database = sqlite3.connect(databasePath)
     c = database.cursor()
-    c.execute("""CREATE TABLE specific_products(
+    c.execute("""CREATE TABLE products(
         name text,
         kaufland_id text,
         auchan_id text,
@@ -47,7 +48,7 @@ def createDatabase():
     database.commit()
     database.close()
 
-def addEntry(name, id, kaufland_id = None, auchan_id = None, biedronka_id = None, carrefour_id = None, \
+def addProduct(name, id, kaufland_id = None, auchan_id = None, biedronka_id = None, carrefour_id = None, \
     tags = None, value = None, unit = None, energy = None, fat = None, saturated_fat = None, \
     trans_fat = None, salt = None, carbohydrate = None, fiber = None, sugar = None, protein = None, \
     calcium = None, chromium = None, copper = None, flouride = None, iodine = None, iron = None, \
@@ -55,13 +56,22 @@ def addEntry(name, id, kaufland_id = None, auchan_id = None, biedronka_id = None
 
     database = sqlite3.connect(databasePath)
     c = database.cursor()
-    c.execute("""INSERT INTO specific_products VALUES 
+    c.execute("""INSERT INTO products VALUES 
     (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
     """, (name, kaufland_id, auchan_id, biedronka_id, carrefour_id, tags, value, unit, energy, fat,\
     saturated_fat, trans_fat, salt, carbohydrate, fiber, sugar, protein, calcium, chromium, copper,\
     flouride, iodine, iron, magnesium, manganese, malybdenum, phosphorus, selenium, zinc, id))
     database.commit()
     database.close()
+
+#return added receipt's rowid
+def addReceipt(date, shop = None, image = None):
+    #TODO write me!
+    return True
+
+def addPurchase(product_id, receipt_id, amount_bought, unit):
+    #TODO write me!
+    return True
 
 def deleteEntry(rowid):
     database = sqlite3.connect(databasePath)
@@ -77,16 +87,19 @@ def deleteEntry(rowid):
     database.close()
 
 
-def printAll():
+def printAll(table):
     database = sqlite3.connect(databasePath)
     c = database.cursor()
-    c.execute("SELECT rowid,* FROM products")
+    c.execute("SELECT rowid,* FROM ?", (table))
     rows = c.fetchall()
     for row in rows:
         print(row)
     database.commit()
     database.close()
 
-printAll()  
 
-print("Exit success")
+if __name__ == "__main__":
+    for table in tables:
+        printAll(table)
+        print()
+    print("Exit success")
