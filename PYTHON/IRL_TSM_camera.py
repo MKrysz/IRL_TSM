7a9508999e525a92a5b49732b31c580f60c043b8
MKrysz <mkrysz@student.agh.edu.pl>
2021-08-30 16:52:38 +0200

    Deleted unnecessary functions and wrote readReceipLiveFromCamera() function

--- PYTHON/IRL_TSM_camera.py
+++ PYTHON/IRL_TSM_camera.py
@@ -1,34 +1,63 @@
 from pyzbar import pyzbar
 import cv2
+import pytesseract
+import time
 
-
-def draw_barcode(decoded, image):
-    # n_points = len(decoded.polygon)
-    # for i in range(n_points):
-    #     image = cv2.line(image, decoded.polygon[i], decoded.polygon[(i+1) % n_points], color=(0, 255, 0), thickness=5)
-    image = cv2.rectangle(image, (decoded.rect.left, decoded.rect.top), 
-                            (decoded.rect.left + decoded.rect.width, decoded.rect.top + decoded.rect.height),
-                            color=(0, 255, 0),
-                            thickness=5)
-    return image
+set_language = "POL"
 
 def decode(image):
     # decodes all barcodes from an image
     decoded_objects = pyzbar.decode(image)
+    result = []
     for obj in decoded_objects:
         # draw the barcode
         # print barcode type & data
-        print("Data:", int(obj.data))
+        result.append(int(obj.data))
+    return result
 
+
+
+
+# if __name__ == "__main__":
+#     cap = cv2.VideoCapture(0)
+#     for i in range(100):
+#         # read the frame from the camera
+#         _, frame = cap.read()
+#         # decode detected barcodes & get the image
+#         # that is drawn
+#         decode(frame)
+#         cv2.imshow("img", frame)
+#         if cv2.waitKey(1) == ord("q"):
+#             break
+
+def readFrame(frame):
+    readString = pytesseract.image_to_string(frame)
+    return readString.splitlines()
+
+def readReceiptLiveFromCamera():
+    result = []
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    i = 0
+    while True:
+        _, frame = cap.read()
+        cv2.imshow("live feed", frame)
+        cv2.waitKey(1)
+        buffer = readFrame(frame)
+        print(buffer, len(buffer))
+        if len(buffer) < 4:
+            i += 1
+        else:
+            i = 0
+            result.append(buffer)
+        if i == 5:
+            break
+        print(i)
+    cap.release()
+    cv2.destroyAllWindows()
+    return result
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(0)
-    for i in range(100):
-        # read the frame from the camera
-        _, frame = cap.read()
-        # decode detected barcodes & get the image
-        # that is drawn
-        decode(frame)
-        cv2.imshow("img", frame)
-        if cv2.waitKey(1) == ord("q"):
-            break
+    for string in readReceiptLiveFromCamera():
+        for line in string:
+            print(line)
+        print("\n\n----------------------------\n")
